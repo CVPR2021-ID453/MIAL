7a67b14d2cd87de5d9b5aa8bc475fdb06e7a2f1a
CVPR2021-ID453 <74519689+CVPR2021-ID453@users.noreply.github.com>
2020-11-25 13:15:03 +0800

    Update voc0712.py

--- configs/_base_/voc0712.py
+++ configs/_base_/voc0712.py
@@ -1,7 +1,6 @@
 # dataset settings
 dataset_type = 'VOCDataset'
 data_root = '/DATA/disk1/Datasets/VOCdevkit/'
-# data_root = '/home/wan/Codes/ALDetection/mmdetection/data/VOCdevkit/'
 
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
