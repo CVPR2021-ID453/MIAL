4461bd69af418060e4ddc2fc058a8430cddb7e7b
CVPR2021-ID453 <74519689+CVPR2021-ID453@users.noreply.github.com>
2020-11-26 05:51:32 +0800

    Add files via upload

--- mmdet/utils/active_datasets.py
+++ mmdet/utils/active_datasets.py
@@ -2,66 +2,51 @@ import mmcv
 import numpy as np
 
 
-def get_init_labeled_set(cfg):
+def get_X_L_0(cfg):
     # load dataset anns
     anns = load_ann_list(cfg.data.train.dataset.ann_file)
-
     # get all indexes
-    all_indexes = np.arange(len(anns[0]) + len(anns[1]))
-
-    # random select labeled set
-    np.random.shuffle(all_indexes)
-    labeled_set = all_indexes[:cfg.active_learning.init_num].copy()
-    unlabeled_set = all_indexes[cfg.active_learning.init_num:cfg.active_learning.init_num*2].copy()
-    labeled_set.sort()
-    unlabeled_set.sort()
-
-    return labeled_set, unlabeled_set, all_indexes, anns
+    X_all = np.arange(len(anns[0]) + len(anns[1]))
+    # randomly select labeled set
+    np.random.shuffle(X_all)
+    X_L = X_all[:cfg.X_L_0_size].copy()
+    X_U = X_all[cfg.X_L_0_size:cfg.X_L_0_size*2].copy()
+    X_L.sort()
+    X_U.sort()
+    return X_L, X_U, X_all, anns
 
 
-def create_active_labeled_set(cfg, labeled_set, anns, cycle):
+def create_X_L_file(cfg, X_L, anns, cycle):
     # split labeled set into 2007 and 2012
-    selected_sets_labeled = [
-        labeled_set[labeled_set < len(anns[0])],
-        labeled_set[labeled_set >= len(anns[0])] - len(anns[0])
-    ]
-
+    X_L = [X_L[X_L < len(anns[0])], X_L[X_L >= len(anns[0])] - len(anns[0])]
     # create labeled ann files
-    active_labeled_path = []
-    for ann, selected_set_labeled, year in zip(anns, selected_sets_labeled, ['07', '12']):
-        save_folder = cfg.work_dir + '/cycle' + str(cycle)
+    X_L_path = []
+    for ann, X_L_single, year in zip(anns, X_L, ['07', '12']):
+        save_folder = cfg.work_direction + '/cycle' + str(cycle)
         mmcv.mkdir_or_exist(save_folder)
-        save_path = save_folder + '/trainval_l' + year + '.txt'
-        np.savetxt(save_path, ann[selected_set_labeled], fmt='%s')
-        active_labeled_path.append(save_path)
-
+        save_path = save_folder + '/trainval_X_L_' + year + '.txt'
+        np.savetxt(save_path, ann[X_L_single], fmt='%s')
+        X_L_path.append(save_path)
     # update cfg
-    cfg.data.train.dataset.ann_file = active_labeled_path
-    cfg.data.train.times = cfg.l_repeat
-
+    cfg.data.train.dataset.ann_file = X_L_path
+    cfg.data.train.times = cfg.X_L_repeat
     return cfg
 
 
-def create_active_unlabeled_set(cfg, unlabeled_set, anns, cycle):
-    # split labeled set into 2007 and 2012
-    selected_sets_unlabeled = [
-        unlabeled_set[unlabeled_set < len(anns[0])],
-        unlabeled_set[unlabeled_set >= len(anns[0])] - len(anns[0])
-    ]
-
+def create_X_U_file(cfg, X_U, anns, cycle):
+    # split unlabeled set into 2007 and 2012
+    X_U = [X_U[X_U < len(anns[0])], X_U[X_U >= len(anns[0])] - len(anns[0])]
     # create labeled ann files
-    active_unlabeled_path = []
-    for ann, selected_set_unlabeled, year in zip(anns, selected_sets_unlabeled, ['07', '12']):
-        save_folder = cfg.work_dir + '/cycle' + str(cycle)
+    X_U_path = []
+    for ann, X_U_single, year in zip(anns, X_U, ['07', '12']):
+        save_folder = cfg.work_direction + '/cycle' + str(cycle)
         mmcv.mkdir_or_exist(save_folder)
-        save_path = save_folder + '/trainval_u' + year + '.txt'
-        np.savetxt(save_path, ann[selected_set_unlabeled], fmt='%s')
-        active_unlabeled_path.append(save_path)
-
+        save_path = save_folder + '/trainval_X_U_' + year + '.txt'
+        np.savetxt(save_path, ann[X_U_single], fmt='%s')
+        X_U_path.append(save_path)
     # update cfg
-    cfg.data.train.dataset.ann_file = active_unlabeled_path
-    cfg.data.train.times = cfg.u_repeat
-
+    cfg.data.train.dataset.ann_file = X_U_path
+    cfg.data.train.times = cfg.X_U_repeat
     return cfg
 
 
@@ -72,19 +57,16 @@ def load_ann_list(paths):
     return anns
 
 
-def update_labeled_set(active_metric, all_set, labeled_set, budget):
-
-    values = active_metric.cpu().numpy()
-    all_unlabeled_set = np.array(list(set(all_set) - set(labeled_set)))
-    unlabeled_values = values[all_unlabeled_set]
-    arg = unlabeled_values.argsort()
-    new_labeled_set = all_unlabeled_set[arg[-budget:]]
-
-    labeled_set = np.concatenate((labeled_set, new_labeled_set))
-    all_unlabeled_set = np.array(list(set(all_set) - set(labeled_set)))
-    np.random.shuffle(all_unlabeled_set)
-    unlabeled_set = all_unlabeled_set[:labeled_set.shape[0]]
-    labeled_set.sort()
-    unlabeled_set.sort()
-
-    return labeled_set, unlabeled_set
+def update_X_L(uncertainty, X_all, X_L, X_S_size):
+    uncertainty = uncertainty.cpu().numpy()
+    all_X_U = np.array(list(set(X_all) - set(X_L)))
+    uncertainty_X_U = uncertainty[all_X_U]
+    arg = uncertainty_X_U.argsort()
+    X_S = all_X_U[arg[-X_S_size:]]
+    X_L_next = np.concatenate((X_L, X_S))
+    all_X_U_next = np.array(list(set(X_all) - set(X_L_next)))
+    np.random.shuffle(all_X_U_next)
+    X_U_next = all_X_U_next[:X_L_next.shape[0]]
+    X_L_next.sort()
+    X_U_next.sort()
+    return X_L_next, X_U_next
